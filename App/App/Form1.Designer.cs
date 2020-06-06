b113b5fd2bed09e33ba2f3599eea42afb0899ccf
pixipit <61790959+pixipit@users.noreply.github.com>
2020-06-06 18:35:10 +0200

    PageTab added

--- App/App/Form1.Designer.cs
+++ App/App/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Addbutton = new System.Windows.Forms.Button();
+            this.FileButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.SuspendLayout();
+            // 
+            // Addbutton
+            // 
+            this.Addbutton.Location = new System.Drawing.Point(88, 12);
+            this.Addbutton.Name = "Addbutton";
+            this.Addbutton.Size = new System.Drawing.Size(70, 30);
+            this.Addbutton.TabIndex = 3;
+            this.Addbutton.Text = "Add";
+            this.Addbutton.UseVisualStyleBackColor = true;
+            this.Addbutton.Click += new System.EventHandler(this.Addbutton_Click);
+            // 
+            // FileButton
+            // 
+            this.FileButton.Location = new System.Drawing.Point(12, 12);
+            this.FileButton.Name = "FileButton";
+            this.FileButton.Size = new System.Drawing.Size(70, 30);
+            this.FileButton.TabIndex = 4;
+            this.FileButton.Text = "File";
+            this.FileButton.UseVisualStyleBackColor = true;
+            this.FileButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "HelloThere";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl
+            // 
+            this.tabControl.ItemSize = new System.Drawing.Size(80, 25);
+            this.tabControl.Location = new System.Drawing.Point(164, 48);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(982, 610);
+            this.tabControl.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1158, 671);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FileButton);
+            this.Controls.Add(this.Addbutton);
+            this.Name = "Form1";
+            this.Text = "App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button Addbutton;
+        private System.Windows.Forms.Button FileButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl;
     }
 }
 
--- App/App/Form1.cs
+++ App/App/Form1.cs
@@ -16,5 +16,33 @@ namespace App
         {
             InitializeComponent();
         }
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // this.WindowState = FormWindowState.Maximized;
+            CreateAPage("HomePage");
+        }
+
+        private void Addbutton_Click(object sender, EventArgs e)
+        {
+            CreateAPage("Page" + tabControl.TabCount);
+        }
+        public void CreateAPage(string name)
+        {
+            //page
+            TabPage tabPage = new TabPage(name);
+            tabPage.Name = name;
+            tabPage.BackColor = Color.DarkGray;
+            //Text
+            Label header = new Label();
+            header.Text = name;
+            tabPage.Controls.Add(header);
+
+            tabControl.Controls.Add(tabPage);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
